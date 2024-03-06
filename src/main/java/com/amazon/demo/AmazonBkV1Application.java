37d75ae0ca5925ef9deb68622f328b25986b2e78
rubini77 <rubinilaxshmi77@gmail.com>
2024-03-06 12:25:39 +0530

    First Commit

---
+++ src/main/java/com/amazon/demo/AmazonBkV1Application.java
@@ -0,0 +1,13 @@
+package com.amazon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonBkV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonBkV1Application.class, args);
+	}
+
+}
